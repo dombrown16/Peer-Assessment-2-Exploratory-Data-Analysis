5f88ce96df7c78a745ac13349b30177088796014
dombrown16 <dombrown_16@hotmail.com>
2016-06-19 16:56:11 +0200

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,4 +1,4 @@
-# Script for plot1 to answer the question:
+# Plot1.R script to answer the question:
 # Have total emissions from PM2.5 decreased in the United States from 
 # 1999 to 2008? Using the base plotting system, make a plot showing the total 
 # PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 
